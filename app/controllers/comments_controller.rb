2a9515c042c77cc07b5a31a7be4532d8321f3cc3
Ainehanta <ainehanta@hotmail.co.jp>
2014-07-08 00:27:58 +0900

    Typo premit -> permit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,6 @@ class CommentsController < ApplicationController
 
   private
     def comment_params
-      params.require(:comment).premit(:commenter, :body)
+      params.require(:comment).permit(:commenter, :body)
     end
 end
