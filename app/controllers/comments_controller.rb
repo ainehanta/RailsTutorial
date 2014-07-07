f180680733d5bb5cb9890284d19b74b1d2b2471a
Ainehanta <ainehanta@hotmail.co.jp>
2014-07-08 00:14:00 +0900

    Add action comments#create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_prams)
+    redirect_to article_path(@article)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).premit(:commenter, :body)
+    end
 end
